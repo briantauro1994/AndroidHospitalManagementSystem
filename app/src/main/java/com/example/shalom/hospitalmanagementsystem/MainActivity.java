4b0c89b30bea3f99667fa713a24afbe2b2ba0a0a
shalom <briantauro7@gmail.com>
2016-04-01 17:29:35 +0530

    Update

--- app/src/main/java/com/example/shalom/hospitalmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/shalom/hospitalmanagementsystem/MainActivity.java
@@ -2,11 +2,10 @@ package com.example.shalom.hospitalmanagementsystem;
 
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -26,7 +25,6 @@ import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -76,118 +74,120 @@ new AsyncHttpTask().execute();
         return result.toString();
     }
 
- class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
-
-String user=username.getText().toString();
-     String pass=password.getText().toString();
-    @Override
-    protected Integer doInBackground(String... params) {
-        InputStream inputStream = null;
-
-        HttpURLConnection urlConnection = null;
-
-        Integer result = 0;
-        try {
-
-                /* forming th java.net.URL object */
-            URL url = new URL(params[0]);
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("POST");
-            urlConnection.setRequestProperty("Content-Type", "application/json");
-
-                /* optional request header */
-            urlConnection.setRequestProperty("Accept", "application/json");
-
-            LoginDto loginDto=null;
-
-               loginDto=new LoginDto();
-                loginDto.setName(user);
-           loginDto.setPassword(pass);
-
-
-            String s=    new Gson().toJson(loginDto);
-
-            OutputStream os = urlConnection.getOutputStream();
-            BufferedWriter writer = new BufferedWriter(
-                    new OutputStreamWriter(os, "UTF-8"));
-           /* HashMap<String, String> postDataParams=new HashMap<>();
-            postDataParams.put("name","brian");
-            postDataParams.put("age", "21");
-            Log.d("tttttttttttt", getPostDataString(postDataParams));*/
-            //writer.write(getPostDataString(postDataParams));
-            writer.write(s);
-            writer.flush();
-            writer.close();
-            os.close();
-            // urlConnection.connect();
-            int statusCode = urlConnection.getResponseCode();
-
-                /* 200 represents HTTP OK */
-            if (statusCode ==  200) {
-
-                inputStream = new BufferedInputStream(urlConnection.getInputStream());
-
-                String response = convertInputStreamToString(inputStream);
-
-                parseResult(response);
-
-                result = 1; // Successful
-
-            }else{
-                result = 0; //"Failed to fetch data!";
-            }
-
-        } catch (Exception e) {
-            Log.d(TAG + "hello", e.getMessage() + "..................");
-        }
-
-        return result; //"Failed to fetch data!";
-    }
-
-
-    @Override
-    protected void onPostExecute(Integer result) {
-            /* Download complete. Lets update UI */
-
-        if(result == 1){
-
-            intent=new Intent(getApplicationContext(),Home.class);
-            startActivity(intent);
-        }else{
-            Log.e(TAG, "Failed to fetch data!");
-        }
-    }
-}
-
-
     private String convertInputStreamToString(InputStream inputStream) throws IOException {
 
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
         String line = "";
         String result = "";
 
-        while((line = bufferedReader.readLine()) != null){
+        while ((line = bufferedReader.readLine()) != null) {
             result += line;
         }
 
             /* Close Stream */
-        if(null!=inputStream){
+        if (null != inputStream) {
             inputStream.close();
         }
 
         return result;
     }
+
     private void parseResult(String result) {
         /* JSONObject response = new JSONObject(result);
 
             JSONArray posts = response.optJSONArray("posts");*/
 
-        Type type = new TypeToken<List<StringDto>>(){}.getType();
+        Type type = new TypeToken<List<StringDto>>() {
+        }.getType();
         StringDto in = new Gson().fromJson(result, type);
 
-        Log.d(TAG,in.getMessage());
+        Log.d(TAG, in.getMessage());
 
+    }
+
+    class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
+
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            InputStream inputStream = null;
+
+            HttpURLConnection urlConnection = null;
+
+            Integer result = 0;
+            try {
+
+                /* forming th java.net.URL object */
+                URL url = new URL(params[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                /* optional request header */
+                urlConnection.setRequestProperty("Accept", "application/json");
+
+                LoginDto loginDto = null;
+
+                loginDto = new LoginDto();
+                loginDto.setName(user);
+                loginDto.setPassword(pass);
+
+
+                String s = new Gson().toJson(loginDto);
+
+                OutputStream os = urlConnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+           /* HashMap<String, String> postDataParams=new HashMap<>();
+            postDataParams.put("name","brian");
+            postDataParams.put("age", "21");
+            Log.d("tttttttttttt", getPostDataString(postDataParams));*/
+                //writer.write(getPostDataString(postDataParams));
+                writer.write(s);
+                writer.flush();
+                writer.close();
+                os.close();
+                // urlConnection.connect();
+                int statusCode = urlConnection.getResponseCode();
+
+                /* 200 represents HTTP OK */
+                if (statusCode == 200) {
+
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+                    String response = convertInputStreamToString(inputStream);
+
+                    parseResult(response);
+
+                    result = 1; // Successful
+
+                } else {
+                    result = 0; //"Failed to fetch data!";
+                }
+
+            } catch (Exception e) {
+                Log.d(TAG + "hello", e.getMessage() + "..................");
+            }
+
+            return result; //"Failed to fetch data!";
+    }
+
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            /* Download complete. Lets update UI */
+
+            if (result == 1) {
+
+                intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+            } else {
+                Log.e(TAG, "Failed to fetch data!");
+            }
+        }
     }
 }
